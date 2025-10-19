cd5dfb2f8de71644bbd903c8f9f2337c4de7400c
Dan Arhipov <chifmatsuno17@gmail.com>
2025-10-19 14:21:41 +0300

    Initial commit

--- postcss.config.js
+++ postcss.config.js
@@ -3,23 +3,22 @@ import postcssPresetEnv from 'postcss-preset-env'
 
 export default ({ env }) => {
   const isProd = env === 'production'
-  /**
-   * @see https://postcss.org/docs/postcss-plugins
-   */
-  const plugins = []
-
-  if (isProd) {
-    plugins.push(
-      postcssPxToRem({
-        propList: ['*'],
-        mediaQuery: true,
-      })
-    )
-
-    plugins.push(postcssPresetEnv())
-  }
 
   return {
-    plugins,
+    plugins: [
+      postcssPresetEnv({
+        stage: 2,
+        features: {
+          'nesting-rules': true,
+          'custom-media-queries': true,
+        }
+      }),
+      ...(isProd ? [
+        postcssPxToRem({
+          propList: ['*'],
+          mediaQuery: true,
+        })
+      ] : [])
+    ]
   }
 }
